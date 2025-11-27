d5e3eca2ce77b1d37184eeb5a73e9ce89a874f73
ramy <kheliliramy0@gmail.com>
2025-11-27 17:30:57 +0100

    repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, reps) {
+    if (reps<0) return 'error';
+    let string = '';
+    for (let i =0 ; i <reps;i++){
+        string += str;
+    }
+    return string
 };
 
 // Do not edit below this line
